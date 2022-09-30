ab317af165081a5f7b377d9c4ea949095c32816b
omrozmen <omrozmen@hotmail.com>
2022-09-30 18:54:43 +0300

    Odev2
    Deneme

--- js/common.js
+++ js/common.js
@@ -18,24 +18,20 @@ const renderHeader = () => {
 				<div class="collapse navbar-collapse" id="navbarNav">
 					<ul class="navbar-nav">
 						<li class="nav-item">
-							<a class="nav-link ${
-								activePage.includes("index.html") ? "active" : ""
-							}" href="index.html">Ana Sayfa</a>
+							<a class="nav-link ${activePage.includes("index.html") ? "active" : ""
+		}" href="index.html">Ana Sayfa</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link ${
-								activePage.includes("about.html") ? "active" : ""
-							}" href="about.html">Hakkımızda</a>
+							<a class="nav-link ${activePage.includes("about.html") ? "active" : ""
+		}" href="about.html">Hakkımızda</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link ${
-								activePage.includes("contact.html") ? "active" : ""
-							}" href="contact.html">İletişim</a>
+							<a class="nav-link ${activePage.includes("contact.html") ? "active" : ""
+		}" href="contact.html">İletişim</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link ${
-								activePage.includes("todos.html") ? "active" : ""
-							}" href="todos.html">Yapılacaklar</a>
+							<a class="nav-link ${activePage.includes("todos.html") ? "active" : ""
+		}" href="todos.html">Yapılacaklar</a>
 						</li>
 					</ul>
 				</div>
--- js/todos.js
+++ js/todos.js
@@ -5,20 +5,26 @@ const root = document.querySelector("#root");
 const editModal = document.querySelector("#editModal");
 let todos = [];
 let todo;
+let currentPage = 1;
+let rows = 30;
 
-const renderTodos = () => {
+
+//#region HtmlTable
+const renderTodos = (page = 1) => {
 	root.innerHTML = "";
 	// todoları listele
 	const table = document.createElement("table");
 	table.setAttribute("class", "table table-hover");
 
 	const thead = document.createElement("thead");
+
+	// Sıralama için title-sorting
 	thead.innerHTML = `
     <tr>
       <th scope="col">id</th>
-      <th scope="col">Başlık</th>
-      <th scope="col">Kullanıcı Id</th>
-      <th scope="col">Durum</th>
+      <th scope="col" id="title-sorting"> Başlık <button class="btnd">&darr;</button> <button class="btna">&uarr;</button></th>
+      <th scope="col" id="userid-sorting">Kullanıcı Id <button class="btnd">&darr;</button> <button class="btna">&uarr;</button></th>
+      <th scope="col" id="status-sorting">Durum <button class="btnd">&darr;</button> <button class="btna">&uarr;</button></th>
       <th scope="col"></th>
     </tr>
   `;
@@ -33,32 +39,39 @@ const renderTodos = () => {
       <td>${item.userId}</td>
       <td>${item.completed ? "Tamamlandı" : "Yapılacak"}</td>
       <td>
-        <button class="btn btn-xs btn-danger remove" data-id=${
-					item.id
-				}>Sil</button>
-        <button class="btn btn-xs btn-warning edit" data-id=${
-					item.id
-				}>Düzenle</button>
+        <button class="btn btn-xs btn-danger remove" data-id=${item.id
+			}>Sil</button>
+        <button class="btn btn-xs btn-warning edit" data-id=${item.id
+			}>Düzenle</button>
       </td>
     `;
 		tbody.appendChild(tr);
 	};
-	todos.slice(0, 15).forEach((item) => {
+	//#endregion
+	page--;
+
+	let start = rows * page;
+	let end = start + rows;
+	let paginatedItems = todos.slice(start, end);
+
+	paginatedItems.forEach((item) => {
 		renderItem(item);
 	});
 	table.appendChild(tbody);
 	root.append(table);
 
+	//#region RemoveMethod
 	document.querySelectorAll(".remove").forEach((button) => {
 		button.addEventListener("click", (e) => {
 			const id = Number(e.currentTarget.getAttribute("data-id"));
 			if (confirm("kaydı silmek istediğinize emin misiniz?")) {
 				todos = todos.filter((x) => x.id !== id);
-				renderTodos();
+				renderTodos(currentPage);
 			}
 		});
 	});
-
+	//#endregion
+	//#region Edit
 	document.querySelectorAll(".edit").forEach((button) => {
 		button.addEventListener("click", (e) => {
 			const id = Number(e.currentTarget.getAttribute("data-id"));
@@ -69,7 +82,114 @@ const renderTodos = () => {
 			editModal.classList.add("show");
 		});
 	});
+	//#endregion
+	// console.log(typeof ("#title-sorting"))
+
+	//#region With TitleSorting
+	document.querySelector('#title-sorting .btnd').addEventListener('click', () => {
+		todos.sort((a, b) => {
+			const nameA = a.title.toUpperCase();
+			const nameB = b.title.toUpperCase();
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+		renderTodos(currentPage);
+	})
+
+	document.querySelector('#title-sorting .btna').addEventListener('click', () => {
+		todos.sort((a, b) => {
+			const nameA = a.title.toUpperCase();
+			const nameB = b.title.toUpperCase();
+			if (nameA > nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+		renderTodos(currentPage);
+	})
+	//#endregion
+	//#region With Status
+	document.querySelector('#status-sorting .btnd').addEventListener('click', () => {
+		todos.sort((a, b) => {
+			const nameA = a.completed;
+			const nameB = b.completed;
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+		renderTodos(currentPage);
+	})
+	document.querySelector('#status-sorting .btna').addEventListener('click', () => {
+		todos.sort((a, b) => {
+			const nameA = a.completed;
+			const nameB = b.completed;
+			if (nameA > nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+		renderTodos(currentPage);
+	})
+	//#endregion
+	//#region  With UserId
+	document.querySelector('#userid-sorting .btna').addEventListener('click', () => {
+		todos.sort((a, b) => {
+			const nameA = a.userId;
+			const nameB = b.userId;
+			if (nameA > nameB) {
+				return -1;
+			}
+			if (nameA < nameB) {
+				return 1;
+			}
+			return 0;
+		});
+		renderTodos(currentPage);
+	})
+
+	document.querySelector('#userid-sorting .btnd').addEventListener('click', () => {
+		todos.sort((a, b) => {
+			const nameA = a.userId;
+			const nameB = b.userId;
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+		renderTodos(currentPage);
+	})
+	//#endregion
 };
+
+
+//#region Html Add Pagination
+document.querySelectorAll(".page-link").forEach((btn) => {
+	btn.addEventListener('click', () => {
+		let data_id = btn.getAttribute('data-id');
+		currentPage = Number(data_id);
+		renderTodos(currentPage);
+	});
+});
+//#endregion
+//#region ModalSaveMethod
 
 editModal.querySelector("#save").addEventListener("click", () => {
 	todo.title = editModal.querySelector("#title").value;
@@ -80,14 +200,16 @@ editModal.querySelector("#save").addEventListener("click", () => {
 	editModal.style.display = "none";
 	editModal.classList.remove("show");
 });
-
+//#endregion
+//#region ModalClose
 editModal.querySelectorAll(".close").forEach((button) => {
 	button.addEventListener("click", () => {
 		editModal.style.display = "none";
 		editModal.classList.remove("show");
 	});
 });
-
+//#endregion
+//#region Fetch API
 fetch(todosUrl)
 	.then((resp) => resp.json())
 	.then((data = []) => {
@@ -97,10 +219,12 @@ fetch(todosUrl)
 	.catch((error) => {
 		errorLogger(error);
 	});
-
-	// sıralama ödevi algoritması
-	// table thead kısmındaki sıralama yapılacak kolonlara event listener eklenecek.
-	// event listener hangi kolon için tıklanıyorsa
-	// sort metodu kullanılarak sıralama yapılacak
-	// sıralanmış todos'todus içerisine atılacak
-	// renderTodos metodu çalıştırılacak.
+		//#endregion
+//#region Ödev +++
+// sıralama ödevi algoritması +++
+// table thead kısmındaki sıralama yapılacak kolonlara event listener eklenecek. ++
+// event listener hangi kolon için tıklanıyorsa ++
+// sort metodu kullanılarak sıralama yapılacak
+// sıralanmış todos'todus içerisine atılacak
+// renderTodos metodu çalıştırılacak.
+//#endregion
